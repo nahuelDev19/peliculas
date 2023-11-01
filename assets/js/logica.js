5775838f2d305791b5919e04a601fe663d921fd0
nahuelDev19 <nahuel.perea.dev@gmail.com>
2023-11-01 18:46:45 -0300

    v1 js

---
+++ assets/js/logica.js
@@ -0,0 +1,7 @@
+let nombre= prompt("cual es tu nombre?")
+
+while(nombre===null || nombre.trim() === "" ){
+    nombre= prompt("cual es tu nombre?")
+    }
+
+alert(`hola ${nombre} te damos la bienvenida a repelis.`)
